89b278c399c051eb045c435128a9cb855176506c
GregSym <gtsrex@gmail.com>
2021-09-18 21:01:27 +0200

    early multithreading testing

--- src/templates/event_loop_item.py
+++ src/templates/event_loop_item.py
@@ -1,5 +1,6 @@
 
 from abc import ABC
+from concurrent.futures import ProcessPoolExecutor
 
 
 class EventLoopItem(ABC):
@@ -14,6 +15,11 @@ class EventLoopItem(ABC):
              - can be called as part of __init__()
         """
 
+    def loop_logic():
+        """
+            update and draw are called iteratively inside this function
+        """
+
     def update():
         """
             The main method to be called iteratively as part of the 
@@ -26,3 +32,24 @@ class EventLoopItem(ABC):
             because the graphics API shouldn't be too deeply entangled with
             regular business logic
         """
+class EventLoopImplementation(EventLoopItem):
+    """
+        An implementation of a basic event-loop
+        - triggered by using the run() -> None method
+    """
+
+    def loop_logic(self):
+        self.update()
+        self.draw()
+
+    def run(self):
+        """ runs the event loop """
+        self.setup()
+        while True:
+            self.loop_logic()
+
+class EventLoopMultithreadedDraw(EventLoopImplementation):
+    def loop_logic(self):
+        with ProcessPoolExecutor() as executor:
+            p1 = executor.submit(lambda: self.update())
+            p2 = executor.submit(lambda: self.draw())
