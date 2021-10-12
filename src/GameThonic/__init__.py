455672b1c5dab96d13316b3cb2eb820ab131e52c
GregSym <gtsrex@gmail.com>
2021-10-12 04:26:13 +0200

    streamlined the tree with top level init

---
+++ src/GameThonic/__init__.py
@@ -0,0 +1,4 @@
+from gamethonic.main import MainApp
+from gamethonic.main import Engine
+
+__all__ = ['MainApp', 'Engine']
--- tests/test_top_level.py
+++ tests/test_top_level.py
@@ -1,11 +1,11 @@
 
 import pygame
-from gamethonic import main
+from gamethonic import Engine
 import pytest
 
 
 def test_top_level():
     """ runs through a single iteration of the event-loop, after setting up a defualt scene """
-    engine = main.Engine()
+    engine = Engine()
     with pytest.raises(pygame.error):
         engine.loop_logic()
