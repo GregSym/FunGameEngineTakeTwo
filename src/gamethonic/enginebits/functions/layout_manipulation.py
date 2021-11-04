c7ebd87a8584cda8c9d72645bdb0bb9884e8c378
GregSym <gtsrex@gmail.com>
2021-11-04 23:09:42 +0200

    added some layoutmanipulation helpers

--- src/gamethonic/enginebits/functions/layout_manipulation.py
+++ src/gamethonic/enginebits/functions/layout_manipulation.py
@@ -5,15 +5,11 @@ from pygame.rect import Rect
 class LayoutManipulation:
 
     @staticmethod
-    def grid_from_container(container_rect: Rect, grid_width: int, grid_height: int) -> list[Rect]:
+    def grid_from_container(container_rect: Rect, grid_width: int, grid_height: int) -> Generator[Rect, None, None]:
+        """ generates a grid from a given surface for specified dimensions """
         cell_width = int(container_rect.width / grid_width)
         cell_height = int(container_rect.height / grid_height)
 
-        def _generate_row() -> Generator[Rect, None, None]:
-            for i in range(cell_height):
-                for j in range(cell_width):
-                    yield Rect(container_rect.left + j*cell_width, container_rect.right + i*cell_height, cell_width, cell_height)
-
-        cells: list[Rect] = list(_generate_row())
-
-        return cells
+        for i in range(cell_height):
+            for j in range(cell_width):
+                yield Rect(container_rect.left + j*cell_width, container_rect.right + i*cell_height, cell_width, cell_height)
