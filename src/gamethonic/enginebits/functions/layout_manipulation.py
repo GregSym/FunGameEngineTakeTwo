06f49cf2bfa36e9639a268baacd3d7230fdf68ae
GregSym <gtsrex@gmail.com>
2021-11-04 06:31:16 +0200

    typing and linting corrections

--- src/gamethonic/enginebits/functions/layout_manipulation.py
+++ src/gamethonic/enginebits/functions/layout_manipulation.py
@@ -1,17 +1,19 @@
 from typing import Generator
-from pygame.math import Vector2
 from pygame.rect import Rect
+
 
 class LayoutManipulation:
 
     @staticmethod
-    def grid_from_container(container_rect: Rect, grid_width: int, grid_height: int) -> list[Vector2]:
-        cell_width = container_rect.width / grid_width
-        cell_height = container_rect.height / grid_height
+    def grid_from_container(container_rect: Rect, grid_width: int, grid_height: int) -> list[Rect]:
+        cell_width = int(container_rect.width / grid_width)
+        cell_height = int(container_rect.height / grid_height)
 
-        def _generate_row() -> Generator[Rect]:
+        def _generate_row() -> Generator[Rect, None, None]:
             for i in range(cell_height):
                 for j in range(cell_width):
                     yield Rect(container_rect.left + j*cell_width, container_rect.right + i*cell_height, cell_width, cell_height)
-        
+
         cells: list[Rect] = list(_generate_row())
+
+        return cells
