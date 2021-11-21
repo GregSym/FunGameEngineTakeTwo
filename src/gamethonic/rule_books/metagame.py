ae2610854d7792817a3a59252b45fa36189f35c3
GregSym <gtsrex@gmail.com>
2021-11-21 07:42:06 +0200

    creating midlevel object to track game rules

---
+++ src/gamethonic/rule_books/metagame.py
@@ -0,0 +1,11 @@
+
+from typing import Protocol
+
+
+class MetaGame(Protocol):
+    """ An interface for game rules and procedures at the top level
+        - 
+    """
+
+    def checkEventsList():
+        ...
