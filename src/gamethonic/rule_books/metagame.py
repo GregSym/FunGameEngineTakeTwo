bafeaa7111da22587df44dcecd30bf52bcd108ec
GregSym <gtsrex@gmail.com>
2021-11-21 08:20:32 +0200

    added metagame abstraction with some goals, type checked+linted

--- src/gamethonic/rule_books/metagame.py
+++ src/gamethonic/rule_books/metagame.py
@@ -4,8 +4,13 @@ from typing import Protocol
 
 class MetaGame(Protocol):
     """ An interface for game rules and procedures at the top level
-        - 
+        - analyses stuff like basic layouts (for games that use grid systems)
+        - calculates score
+        - can be used to control positioning of GameObjects as an alternative
+        to physics driven positioning - _ideally_ one wouldn't handle physics
+        in this particular abstraction but it should be possible to do, from
+        a design goal perspective
     """
 
-    def checkEventsList():
+    def checkEventsList(self):
         ...
